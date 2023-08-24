3ab2fb6e79e629b8e0a69e2f42cee7ce7801a43b
Daniel Pareja Londoño <dpareja1394@gmail.com>
2023-08-24 17:07:47 -0500

    Generacion Clases Independientes

---
+++ 2023_2/tecnicas/Back/store/src/main/java/co/edu/usbcali/store/domain/EstadoPedido.java
@@ -0,0 +1,25 @@
+package co.edu.usbcali.store.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "estadospedido")
+public class EstadoPedido {
+
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 10)
+    private String descripcion;
+
+}
---
+++ 2023_2/tecnicas/Back/store/src/main/java/co/edu/usbcali/store/domain/TipoDocumento.java
@@ -0,0 +1,25 @@
+package co.edu.usbcali.store.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tiposdocumento")
+public class TipoDocumento {
+
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 10)
+    private String descripcion;
+
+}
