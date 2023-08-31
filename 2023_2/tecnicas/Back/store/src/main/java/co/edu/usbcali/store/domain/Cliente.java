9331c876a9196db164924be10af1a5953eec311f
Daniel Pareja Londoño <dpareja1394@gmail.com>
2023-08-31 15:34:36 -0500

    Agregar relación de cliente a tipo de documento

--- 2023_2/tecnicas/Back/store/src/main/java/co/edu/usbcali/store/domain/Cliente.java
+++ 2023_2/tecnicas/Back/store/src/main/java/co/edu/usbcali/store/domain/Cliente.java
@@ -31,4 +31,8 @@ public class Cliente {
     @Column(nullable = false, length = 1)
     private String estado;
 
+    @ManyToOne
+    @JoinColumn(name = "tido_id", referencedColumnName = "id", nullable = false)
+    private TipoDocumento tipoDocumento;
+
 }
