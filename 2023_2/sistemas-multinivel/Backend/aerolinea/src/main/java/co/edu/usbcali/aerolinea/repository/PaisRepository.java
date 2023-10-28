3f677ad034e2bca3b6e918854a50d817e256cfa3
Daniel Pareja Londoño <dpareja1394@gmail.com>
2023-10-28 10:11:11 -0500

    Clase de Entidad Pais, Interfaz de PaisRepository y Configuracion de conexión a DB en Application.properties

---
+++ 2023_2/sistemas-multinivel/Backend/aerolinea/src/main/java/co/edu/usbcali/aerolinea/repository/PaisRepository.java
@@ -0,0 +1,9 @@
+package co.edu.usbcali.aerolinea.repository;
+
+import co.edu.usbcali.aerolinea.domain.Pais;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaisRepository extends JpaRepository<Pais, Integer> {
+}
