7834f19f52912a2a9814ec2719cd8af5bbe558a7
Daniel Pareja Londoño <dpareja1394@gmail.com>
2023-11-09 16:20:56 -0500

    Inserts para pedidos

--- 2023_2/tecnicas/DB/Inserts.sql
+++ 2023_2/tecnicas/DB/Inserts.sql
@@ -11,3 +11,7 @@ VALUES(1, 'Daniel', 'Pareja Londoño', '101234', 'A');
 
 INSERT INTO categorias (nombre, descripcion) VALUES ('JUGUETES', 'Todo tipo de juguetería infantil');
 INSERT INTO categorias (nombre, descripcion) VALUES ('HOGAR', 'Accesorios para el hogar');
+
+INSERT INTO public.pedidos (clie_id, espe_id, fecha, total) VALUES(1, 1, now(), 20000);
+INSERT INTO public.pedidos (clie_id, espe_id, fecha, total) VALUES(1, 1, now(), 30000);
+INSERT INTO public.pedidos (clie_id, espe_id, fecha, total) VALUES(1, 1, now(), 25000);
